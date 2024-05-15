1b993c42edfa29deb0558dbe9304d9e5d89bb724
VlaShi <moyoMylo>
2024-05-16 00:32:05 +0300

    аад

--- include/tarraytable.h
+++ include/tarraytable.h
@@ -2,7 +2,44 @@
 
 #include "ttable.h"
 
-// таблица, чьи записи хранятся в массиве
+const size_t TAB_MAX_SIZE = 99;
+
+// таблица на основе массива, доступ к элементам через поле CurrPos
 class TArrayTable : public TTable {
-	
+protected:
+	TRecord* Arr;			// указатель на начало массива
+	size_t TabSize;			// текущий размер массива, макс. число
+	int CurrPos;			// (нумерация с 0) для работы с методами итерации
+public:
+	// CONSTRUCTORS & DESTRUCTOR
+
+	TArrayTable(int = 0);							// default constructor
+	TArrayTable(const TRecord&);					// conversion constructor
+	TArrayTable(const TArrayTable&);				// copy constructor
+	~TArrayTable() override;
+
+	// ITERATION METHODS
+
+	void Reset() override;
+	void GoNext() override;
+	bool IsEnd() const noexcept override;
+
+	// GET-SET METHODS
+
+	size_t GetSize() const noexcept;
+	TKey GetKey() const override;
+	TValue GetValue() const override;
+	TRecord GetRecord() const override;
+	int GetCurrPos() const noexcept;
+
+	void SetKey(const TKey&);
+	void SetValue(const TValue&);
+	void SetRecord(const TRecord&);
+	void SetCurrPos(int);
+
+	// OTHERS METHODS
+
+	virtual bool IsFull() const noexcept;
+
+	// FindRecord, InsRecord, DelRecord будут реализованы в классе-потомке
 };
--- include/ttable.h
+++ include/ttable.h
@@ -12,6 +12,7 @@ struct TRecord {
 	TValue val;
 };
 
+// таблица состоит из записей (TRecord), каждая из которых имеет свой уникальный ключ (TKey)
 class TTable {
 protected:
 	int DataCount;
@@ -25,18 +26,17 @@ public:
 	void ClearEff() noexcept { eff = 0; }
 	bool IsEmpty() const noexcept { return DataCount == 0; }
 
-	virtual bool IsFull() = 0;
-	virtual bool Find(const TKey&) = 0;
+	virtual bool FindRecord(const TKey&) = 0;
 	virtual bool InsRecord(const TRecord&) = 0;
 	virtual void DelRecord(const TKey&) = 0;
 
 	virtual void Reset() = 0;
 	virtual void GoNext() = 0;
-	virtual bool IsEnd() = 0;
+	virtual bool IsEnd() const noexcept = 0;
 
-	virtual TKey GetKey() const noexcept = 0;
-	virtual TValue GetValue() const noexcept = 0;
-	virtual TRecord& GetRecord() = 0;
+	virtual TKey GetKey() const  = 0;
+	virtual TValue GetValue() const  = 0;
+	virtual TRecord GetRecord() const  = 0;
 
 
 	friend ostream& operator<<(ostream& os, TTable& t) {
