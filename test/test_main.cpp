2ae2cb4045b46995957ce660155b446c1a8fa972
VlaShi <moyoMylo>
2024-05-23 01:44:49 +0300

    убрал из main-файла test-проекта мусор

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,8 +3,4 @@
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
-#include "../include/ttreetable.h"
-    TTreeTable t(TRecord{ "muhahah" });
-    t.Print();
 }
