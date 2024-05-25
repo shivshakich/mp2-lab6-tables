2bcb2d8d1d125b2982f5943ca26ef8b63d83116b
VlaShi <moyoMylo>
2024-05-25 04:47:42 +0300

    дизайн графического приложения

--- sln/application/MyForm.h
+++ sln/application/MyForm.h
@@ -34,6 +34,46 @@ namespace CppWinForm1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewButtonColumn^ Column1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::RadioButton^ radioButton6;
+	private: System::Windows::Forms::RadioButton^ radioButton5;
+	private: System::Windows::Forms::RadioButton^ radioButton4;
+	private: System::Windows::Forms::RadioButton^ radioButton3;
+	private: System::Windows::Forms::RadioButton^ radioButton2;
+	private: System::Windows::Forms::RadioButton^ radioButton1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Label^ label7;
+
+
+
+
+
+
+
+
+	protected:
+
+
+
 
 	private:
 		/// <summary>
@@ -48,12 +88,335 @@ namespace CppWinForm1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->Column1 = (gcnew System::Windows::Forms::DataGridViewButtonColumn());
+			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton4 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton5 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton6 = (gcnew System::Windows::Forms::RadioButton());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+				this->Column1,
+					this->Column2, this->Column3
+			});
+			this->dataGridView1->Location = System::Drawing::Point(13, 13);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(750, 400);
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// Column1
+			// 
+			this->Column1->HeaderText = L"[DELETE]";
+			this->Column1->MinimumWidth = 6;
+			this->Column1->Name = L"Column1";
+			this->Column1->Width = 75;
+			// 
+			// Column2
+			// 
+			this->Column2->HeaderText = L"KEY";
+			this->Column2->MinimumWidth = 6;
+			this->Column2->Name = L"Column2";
+			this->Column2->ReadOnly = true;
+			this->Column2->Width = 125;
+			// 
+			// Column3
+			// 
+			this->Column3->HeaderText = L"VALUE";
+			this->Column3->MinimumWidth = 6;
+			this->Column3->Name = L"Column3";
+			this->Column3->ReadOnly = true;
+			this->Column3->Width = 400;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(133, 417);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(13, 420);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(114, 16);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Эффективность";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(240, 420);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(77, 16);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Результат";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(323, 417);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(440, 22);
+			this->textBox2->TabIndex = 4;
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::White;
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->radioButton6);
+			this->panel1->Controls->Add(this->radioButton5);
+			this->panel1->Controls->Add(this->radioButton4);
+			this->panel1->Controls->Add(this->radioButton3);
+			this->panel1->Controls->Add(this->radioButton2);
+			this->panel1->Controls->Add(this->radioButton1);
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Location = System::Drawing::Point(16, 445);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(301, 296);
+			this->panel1->TabIndex = 5;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(4, 4);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(91, 16);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Тип таблицы";
+			// 
+			// radioButton1
+			// 
+			this->radioButton1->AutoSize = true;
+			this->radioButton1->Location = System::Drawing::Point(7, 24);
+			this->radioButton1->Name = L"radioButton1";
+			this->radioButton1->Size = System::Drawing::Size(206, 20);
+			this->radioButton1->TabIndex = 1;
+			this->radioButton1->TabStop = true;
+			this->radioButton1->Text = L"Неупорядоченная таблица";
+			this->radioButton1->UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this->radioButton2->AutoSize = true;
+			this->radioButton2->Location = System::Drawing::Point(7, 51);
+			this->radioButton2->Name = L"radioButton2";
+			this->radioButton2->Size = System::Drawing::Size(189, 20);
+			this->radioButton2->TabIndex = 2;
+			this->radioButton2->TabStop = true;
+			this->radioButton2->Text = L"Упорядоченная таблица";
+			this->radioButton2->UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this->radioButton3->AutoSize = true;
+			this->radioButton3->Location = System::Drawing::Point(7, 78);
+			this->radioButton3->Name = L"radioButton3";
+			this->radioButton3->Size = System::Drawing::Size(170, 20);
+			this->radioButton3->TabIndex = 3;
+			this->radioButton3->TabStop = true;
+			this->radioButton3->Text = L"Хэш-таблица (массив)";
+			this->radioButton3->UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			this->radioButton4->AutoSize = true;
+			this->radioButton4->Location = System::Drawing::Point(7, 105);
+			this->radioButton4->Name = L"radioButton4";
+			this->radioButton4->Size = System::Drawing::Size(168, 20);
+			this->radioButton4->TabIndex = 4;
+			this->radioButton4->TabStop = true;
+			this->radioButton4->Text = L"Хэш-таблица (список)";
+			this->radioButton4->UseVisualStyleBackColor = true;
+			// 
+			// radioButton5
+			// 
+			this->radioButton5->AutoSize = true;
+			this->radioButton5->Location = System::Drawing::Point(7, 132);
+			this->radioButton5->Name = L"radioButton5";
+			this->radioButton5->Size = System::Drawing::Size(126, 20);
+			this->radioButton5->TabIndex = 5;
+			this->radioButton5->TabStop = true;
+			this->radioButton5->Text = L"Дерево поиска";
+			this->radioButton5->UseVisualStyleBackColor = true;
+			// 
+			// radioButton6
+			// 
+			this->radioButton6->AutoSize = true;
+			this->radioButton6->Location = System::Drawing::Point(7, 159);
+			this->radioButton6->Name = L"radioButton6";
+			this->radioButton6->Size = System::Drawing::Size(107, 20);
+			this->radioButton6->TabIndex = 6;
+			this->radioButton6->TabStop = true;
+			this->radioButton6->Text = L"АВЛ-дерево";
+			this->radioButton6->UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(4, 280);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(102, 16);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"КЧ-дерева нет";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(323, 445);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(440, 23);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Создать таблицу с заданным количеством записей";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(324, 475);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"В файл";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(405, 475);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(86, 23);
+			this->button3->TabIndex = 8;
+			this->button3->Text = L"Из файла";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(469, 504);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(187, 22);
+			this->textBox3->TabIndex = 9;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(326, 504);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(130, 16);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"Полное имя файла";
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(662, 532);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(101, 23);
+			this->button4->TabIndex = 11;
+			this->button4->Text = L"Поиск";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(326, 537);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(41, 16);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"Ключ";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(469, 533);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(187, 22);
+			this->textBox4->TabIndex = 13;
+			this->textBox4->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox4_TextChanged);
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(469, 562);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(187, 22);
+			this->textBox5->TabIndex = 14;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(662, 560);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(101, 23);
+			this->button5->TabIndex = 15;
+			this->button5->Text = L"Добавить\r\n";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(326, 567);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(66, 16);
+			this->label7->TabIndex = 16;
+			this->label7->Text = L"Полином";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::PaleGoldenrod;
+			this->ClientSize = System::Drawing::Size(782, 753);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void textBox4_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
